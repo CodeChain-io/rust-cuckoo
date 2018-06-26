bfb1e5591abb9e0dc7c55f51578878eaf036a585
Joon-Mo Yang <jmyang@kodebox.io>
2018-06-26 17:36:21 +0900

    Add skeleton for cuckoo cycle

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+pub fn verify(message: Vec<u8>, proof: Vec<u32>) -> bool {
+    unimplemented!()
+}
+
+pub fn solve(message: Vec<u8>, length: usize) -> Option<Vec<u32>> {
+    unimplemented!()
 }
