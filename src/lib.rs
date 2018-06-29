e59205823a9a43dd674f44b08eb93f979657fa51
Joon-Mo Yang <jmyang@kodebox.io>
2018-06-29 11:23:23 +0900

    Create SipHasher24 with blake hash as keys

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,24 @@
+extern crate byteorder;
 extern crate crypto;
+extern crate siphasher;
 
+use byteorder::{ByteOrder, NativeEndian};
 use crypto::digest::Digest;
 use crypto::blake2b::Blake2b;
+use siphasher::sip::SipHasher24;
 
 pub fn verify(message: &[u8], proof: &[u32]) -> bool {
-    let hash = {
-        let mut hasher = Blake2b::new(32);
+    let hasher = {
+        let mut blake_hasher = Blake2b::new(32);
         let mut result = Vec::new();
-        hasher.input(message);
-        hasher.result(&mut result);
-        result
+        blake_hasher.input(message);
+        blake_hasher.result(&mut result);
+        let key_0 = NativeEndian::read_u64(&result[0..8]).to_le();
+        let key_1 = NativeEndian::read_u64(&result[8..16]).to_le();
+
+        SipHasher24::new_with_keys(key_0, key_1)
     };
+
     unimplemented!()
 }
 
