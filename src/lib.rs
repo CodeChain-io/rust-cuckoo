d329d50def8aef478ef9410c9f267c423a73fd8e
Joon-Mo Yang <jmyang@kodebox.io>
2018-06-29 15:29:16 +0900

    Add Cuckoo struct

--- src/lib.rs
+++ src/lib.rs
@@ -7,21 +7,37 @@ use crypto::digest::Digest;
 use crypto::blake2b::Blake2b;
 use siphasher::sip::SipHasher24;
 
-pub fn verify(message: &[u8], proof: &[u32]) -> bool {
-    let hasher = {
-        let mut blake_hasher = Blake2b::new(32);
-        let mut result = Vec::new();
-        blake_hasher.input(message);
-        blake_hasher.result(&mut result);
-        let key_0 = NativeEndian::read_u64(&result[0..8]).to_le();
-        let key_1 = NativeEndian::read_u64(&result[8..16]).to_le();
-
-        SipHasher24::new_with_keys(key_0, key_1)
-    };
-
-    unimplemented!()
+pub struct Cuckoo {
+    max_vertex: usize,
+    max_edge: usize,
+    cycle_length: usize,
 }
 
-pub fn solve(message: Vec<u8>, length: usize) -> Option<Vec<u32>> {
-    unimplemented!()
+impl Cuckoo {
+    pub fn new(max_vertex: usize, max_edge: usize, cycle_length: usize) -> Self {
+        Self {
+            max_vertex,
+            max_edge,
+            cycle_length,
+        }
+    }
+
+    pub fn verify(&self, message: &[u8], proof: &[u32]) -> bool {
+        let hasher = {
+            let mut blake_hasher = Blake2b::new(32);
+            let mut result = Vec::new();
+            blake_hasher.input(message);
+            blake_hasher.result(&mut result);
+            let key_0 = NativeEndian::read_u64(&result[0..8]).to_le();
+            let key_1 = NativeEndian::read_u64(&result[8..16]).to_le();
+
+            SipHasher24::new_with_keys(key_0, key_1)
+        };
+
+        unimplemented!()
+    }
+
+    pub fn solve(&self, message: Vec<u8>) -> Option<Vec<u32>> {
+        unimplemented!()
+    }
 }
