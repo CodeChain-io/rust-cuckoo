d4a93eec5b5295c23067ccc8250dffd1510b36c5
Joon-Mo Yang <jmyang@kodebox.io>
2018-06-29 11:04:52 +0900

    Calculate blake2b hash of input

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,16 @@
-pub fn verify(message: Vec<u8>, proof: Vec<u32>) -> bool {
+extern crate crypto;
+
+use crypto::digest::Digest;
+use crypto::blake2b::Blake2b;
+
+pub fn verify(message: &[u8], proof: &[u32]) -> bool {
+    let hash = {
+        let mut hasher = Blake2b::new(32);
+        let mut result = Vec::new();
+        hasher.input(message);
+        hasher.result(&mut result);
+        result
+    };
     unimplemented!()
 }
 
